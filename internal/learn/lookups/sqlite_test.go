424e39968f7bb7a5adffe12551eaa304585a6e85
Keith Herrington <keith@kodi.tv>
2026-09-03 18:45:10 -0700

    test: initialize sqlite before parallel lookup tests

---
+++ internal/learn/lookups/sqlite_test.go
@@ -0,0 +1,24 @@
+// Copyright 2026 keithah and contributors. Licensed under Apache-2.0. See LICENSE.
+
+package lookups
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+
+	_ "modernc.org/sqlite"
+)
+
+// TestMain initializes modernc SQLite before parallel lookup tests.
+func TestMain(m *testing.M) {
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		panic(err)
+	}
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+	_ = db.Close()
+	os.Exit(m.Run())
+}
